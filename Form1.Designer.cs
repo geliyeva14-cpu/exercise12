d9e1cad502b013aba27818f142c0365466af824c
geliyeva14-cpu <geliyeva14@std.beu.edu.az>
2025-11-12 00:03:01 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Bomb_Rain
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            bomb1 = new PictureBox();
+            bomb2 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            right = new PictureBox();
+            left = new PictureBox();
+            man = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)bomb1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bomb2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)right).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)left).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)man).BeginInit();
+            SuspendLayout();
+            // 
+            // bomb1
+            // 
+            bomb1.Image = (Image)resources.GetObject("bomb1.Image");
+            bomb1.Location = new Point(367, 0);
+            bomb1.Margin = new Padding(3, 2, 3, 2);
+            bomb1.Name = "bomb1";
+            bomb1.Size = new Size(61, 45);
+            bomb1.SizeMode = PictureBoxSizeMode.Zoom;
+            bomb1.TabIndex = 1;
+            bomb1.TabStop = false;
+            // 
+            // bomb2
+            // 
+            bomb2.Image = (Image)resources.GetObject("bomb2.Image");
+            bomb2.Location = new Point(140, 0);
+            bomb2.Margin = new Padding(3, 2, 3, 2);
+            bomb2.Name = "bomb2";
+            bomb2.Size = new Size(61, 45);
+            bomb2.SizeMode = PictureBoxSizeMode.Zoom;
+            bomb2.TabIndex = 2;
+            bomb2.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 200;
+            timer1.Tick += timer1_Tick;
+            // 
+            // right
+            // 
+            right.Image = (Image)resources.GetObject("right.Image");
+            right.Location = new Point(224, 304);
+            right.Margin = new Padding(3, 2, 3, 2);
+            right.Name = "right";
+            right.Size = new Size(110, 119);
+            right.SizeMode = PictureBoxSizeMode.StretchImage;
+            right.TabIndex = 7;
+            right.TabStop = false;
+            // 
+            // left
+            // 
+            left.Image = (Image)resources.GetObject("left.Image");
+            left.Location = new Point(224, 304);
+            left.Margin = new Padding(3, 2, 3, 2);
+            left.Name = "left";
+            left.Size = new Size(110, 119);
+            left.SizeMode = PictureBoxSizeMode.StretchImage;
+            left.TabIndex = 8;
+            left.TabStop = false;
+            // 
+            // man
+            // 
+            man.Image = (Image)resources.GetObject("man.Image");
+            man.Location = new Point(224, 293);
+            man.Margin = new Padding(3, 2, 3, 2);
+            man.Name = "man";
+            man.Size = new Size(110, 130);
+            man.SizeMode = PictureBoxSizeMode.StretchImage;
+            man.TabIndex = 9;
+            man.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(575, 423);
+            Controls.Add(man);
+            Controls.Add(left);
+            Controls.Add(right);
+            Controls.Add(bomb2);
+            Controls.Add(bomb1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
+            MaximumSize = new Size(591, 462);
+            MinimumSize = new Size(591, 462);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bomb rain";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)bomb1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bomb2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)right).EndInit();
+            ((System.ComponentModel.ISupportInitialize)left).EndInit();
+            ((System.ComponentModel.ISupportInitialize)man).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox bomb1;
+        private PictureBox bomb2;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox right;
+        private PictureBox left;
+        private PictureBox man;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System.Media;
+
+namespace Bomb_Rain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        int s1 = 0, s2 = 0, score = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bomb1.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+            bomb2.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+            if (bomb1.Bounds.IntersectsWith(bomb2.Bounds))
+            {
+                Form1_Load(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Game Started use <- and -> , every avoidness is 10 point", "Information", MessageBoxButtons.OK);
+                man.Location = new Point(250, 290);
+                left.Location = new Point(man.Location.X, man.Location.Y + 15);
+                right.Location = new Point(man.Location.X, man.Location.Y + 15);
+                man.Visible = true;
+                left.Visible = false;
+                right.Visible = false;
+                s1 = 0;
+                s2 = 0;
+                score = 0;
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (left.Bounds.IntersectsWith(bomb1.Bounds) || left.Bounds.IntersectsWith(bomb2.Bounds) ||
+               right.Bounds.IntersectsWith(bomb1.Bounds) || right.Bounds.IntersectsWith(bomb2.Bounds) ||
+               man.Bounds.IntersectsWith(bomb1.Bounds) || man.Bounds.IntersectsWith(bomb2.Bounds))
+            {
+                timer1.Stop();
+                SoundPlayer sound = new SoundPlayer("C:\\Users\\user\\Downloads\\explosion.wav");
+                sound.Play();
+                DialogResult d = MessageBox.Show($"Your Score : {score}\n" + $"You Lose , Want to restart?", "Information", MessageBoxButtons.YesNo);
+                if (d == DialogResult.Yes)
+                {
+                    Form1_Load(sender, e);
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            if (bomb1.Location.Y >= 560)
+            {
+                bomb1.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+                if (bomb1.Bounds.IntersectsWith(bomb2.Bounds))
+                {
+                    while (bomb1.Bounds.IntersectsWith(bomb2.Bounds))
+                        bomb1.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+                }
+                s1 = 0;
+                score += 10;
+            }
+            if (bomb2.Location.Y >= 560)
+            {
+                bomb2.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+                if (bomb1.Bounds.IntersectsWith(bomb2.Bounds))
+                {
+                    while (bomb1.Bounds.IntersectsWith(bomb2.Bounds))
+                        bomb2.Location = new Point(r.Next(0, 575), r.Next(0, 100));
+                }
+                s2 = 0;
+                score += 10;
+            }
+            s1++;
+            s2++;
+            bomb1.Top += s1;
+            bomb2.Top += s2;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                man.Visible = false;
+                right.Visible = false;
+                left.Visible = true;
+                if (left.Left - 10 > 0)
+                {
+                    left.Left -= 10;
+                }
+                else
+                {
+                    left.Left = 0;
+                }
+                right.Location = left.Location;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                man.Visible = false;
+                left.Visible = false;
+                right.Visible = true;
+                if (right.Left + 10 < 550)
+                {
+                    right.Left += 10;
+                }
+                else
+                {
+                    right.Left = 550;
+                }
+                left.Location = right.Location;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            man.Location = new Point(left.Location.X, left.Location.Y - 15);
+            if (right.Left == 550)
+            {
+                man.Left = 596;
+            }
+            man.Visible = true;
+            if (left.Visible)
+            {
+                left.Visible = false;
+            }
+            if (right.Visible)
+            {
+                right.Visible = false;
+            }
+        }
+    }
+}
